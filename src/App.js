ed88c949fa425301a0d4116eeb43976a05ec730b
Allan Lenny <owinoallan77@gmail.com>
2023-02-09 13:20:01 +0300

    updated sidebar

--- src/App.js
+++ src/App.js
@@ -6,105 +6,108 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
-  Ecommerce,
-  Orders,
-  Calendar,
-  Employees,
-  Stacked,
-  Pyramid,
-  Customers,
-  Kanban,
-  Area,
-  Bar,
-  Pie,
-  Line,
-  Financial,
-  ColorPicker,
-  ColorMapping,
-  Editor,
+	Ecommerce,
+	Orders,
+	Calendar,
+	Employees,
+	Stacked,
+	Pyramid,
+	Customers,
+	Kanban,
+	Area,
+	Bar,
+	Pie,
+	Line,
+	Financial,
+	ColorPicker,
+	ColorMapping,
+	Editor,
 } from "./pages";
 
+import { useStateContext } from "./contexts/ContextProvider";
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+	const { activeMenu } = useStateContext();
 
-  return (
-    <div>
-      <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent content="Settings" position="Top">
-              <button
-                type="button"
-                className="text-3xl p-3 
+	return (
+		<div>
+			<BrowserRouter>
+				<div className="flex relative dark:bg-main-dark-bg">
+					<div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+						<TooltipComponent content="Settings" position="Top">
+							<button
+								type="button"
+								className="text-3xl p-3 
             hover:drop-shadow-x1
             hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
-              >
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-          </div>
-          {activeMenu ? (
-            <div
-              className="w-72 fixed sidebar
+								style={{ background: "blue", borderRadius: "50%" }}
+							>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+					{activeMenu ? (
+						<div
+							className="w-72 fixed sidebar
           dark:bg-secondary-dark-bg
           bg-white"
-            >
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg"><Sidebar /></div>
-          )}
+						>
+							<Sidebar />
+						</div>
+					) : (
+						<div className="w-0 dark:bg-secondary-dark-bg">
+							<Sidebar />
+						</div>
+					)}
 
-          <div
-            className={
-              activeMenu
-                ? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full"
-                : "dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2"
-            }
-          >
-            <div
-              className="fixed md:static
+					<div
+						className={
+							activeMenu
+								? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full"
+								: "dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2"
+						}
+					>
+						<div
+							className="fixed md:static
             bg-main-bg dark:bg-main-dark-bg
             navbar w-full"
-            >
-              <Navbar />
-            </div>
-          </div>
-          <div>
-            <Routes>
-              {/* Dashboard */}
-              <Route path="/" element={<Ecommerce />} />
-              <Route path="/ecommerce" element={<Ecommerce />} />
+						>
+							<Navbar />
+						</div>
+					</div>
+					<div>
+						<Routes>
+							{/* Dashboard */}
+							<Route path="/" element={<Ecommerce />} />
+							<Route path="/ecommerce" element={<Ecommerce />} />
 
-              {/* pages */}
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/employees" element={<Employees />} />
-              <Route path="/customers" element={<Customers />} />
+							{/* pages */}
+							<Route path="/orders" element={<Orders />} />
+							<Route path="/employees" element={<Employees />} />
+							<Route path="/customers" element={<Customers />} />
 
-              {/* apps */}
-              <Route path="/kanban" element={<Kanban />} />
-              <Route path="/editor" element={<Editor />} />
-              <Route path="/calender" element={<Calendar />} />
-              <Route path="/color-picker" element={<ColorPicker />} />
+							{/* apps */}
+							<Route path="/kanban" element={<Kanban />} />
+							<Route path="/editor" element={<Editor />} />
+							<Route path="/calender" element={<Calendar />} />
+							<Route path="/color-picker" element={<ColorPicker />} />
 
-              {/* charts */}
-              <Route path="/line" element={<Line />} />
-              <Route path="/area" element={<Area />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/financial" element={<Financial />} />
-              <Route path="/color-mapping" element={<ColorMapping />} />
-              <Route path="/pyramid" element={<Pyramid />} />
-              <Route path="/stacked" element={<Stacked />} />
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+							{/* charts */}
+							<Route path="/line" element={<Line />} />
+							<Route path="/area" element={<Area />} />
+							<Route path="/bar" element={<Bar />} />
+							<Route path="/pie" element={<Pie />} />
+							<Route path="/financial" element={<Financial />} />
+							<Route path="/color-mapping" element={<ColorMapping />} />
+							<Route path="/pyramid" element={<Pyramid />} />
+							<Route path="/stacked" element={<Stacked />} />
+						</Routes>
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
 
-import React from 'react';
-import ReactDOM  from 'react-dom';
+import "./index.css";
+import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
 
-import './index.css'
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>,
+	document.getElementById("root")
+);
